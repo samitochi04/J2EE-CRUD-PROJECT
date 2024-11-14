3ccb92ecfe8fed98e685648d7d2703a75cf83740
Samuel FOTSO <126283225+samitochi04@users.noreply.github.com>
2024-11-14 12:22:16 +0100

    CRUD working

--- src/main/java/com/example/demo/J2EeCrudProjectApplication.java
+++ src/main/java/com/example/demo/J2EeCrudProjectApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class J2EeCrudProjectApplication {
-
+public class J2eeCrudProjectApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(J2EeCrudProjectApplication.class, args);
+		/*SpringApplication.run(WebCrudAppApplication.class, args);*/
+		SpringApplication app = new SpringApplication(J2eeCrudProjectApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
+        app.run(args);
 	}
+
 
 }
